b43b86985560b28bdc7b9d16d16d9e0384bf55b9
jackon shiundu <jackonshiundu2019@gmail.com>
2022-09-04 00:16:13 +0300

    second commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
         <div className="flex px-10 text-2xl whitespace-nowrap space-x-10  sm:space-x-20 overflow-x-scroll scrollbar-hide">
            {
                Object.entries(requests).map(([key,{gener,url,title}])=>(
-                <h2 key={key} 
+                <h2 key={gener} 
                 onClick={()=>router.push(`/?gener=${gener}`) }
                 className='last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500'>{title}</h2>
                ))
